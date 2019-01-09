2b44e37d5c4ce53fafbac9d3d3b7623d57ed7f92
arslanashraf7 <arslanashraf051@gmail.com>
2019-01-09 10:57:18 +0000

    Added recipe model

--- app/core/models.py
+++ app/core/models.py
@@ -80,3 +80,25 @@ class Ingredient(models.Model):
         """
 
         return self.name
+
+
+class Recipe(models.Model):
+    """
+    Mpdel class for the recipe objects
+    """
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=255)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+    ingredients = models.ManyToManyField('Ingredient')
+    tags = models.ManyToManyField('Tag')
+
+    def __str__(self):
+        """
+        String representation of the recipe model
+        """
+        return self.title
